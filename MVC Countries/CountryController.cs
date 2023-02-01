2c445b701674243a1d21d0d18b93f9d637e727de
ethan <ethanbjansen@gmail.com>
2023-02-01 16:29:19 -0600

    initial

---
+++ MVC Countries/CountryController.cs
@@ -0,0 +1,48 @@
+﻿using Circle;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Countries
+{
+    internal class CountryController
+    {
+        public List<Country> CountriesDB = new List<Country>()
+        {
+            new Country("Russia", "Europe and Asia",new List<string> {"White","Blue","Red"} ),
+             new Country("Canada", "North America",new List<string> {"Red","White"} ),
+              new Country("China", "Asia",new List<string> {"Red","Yellow"} ),
+               new Country("Brazil", "South America",new List<string> {"Green","Yellow","Blue"} ),
+                new Country("Australia", "Oceania",new List<string> {"Red","White","Blue"} ),
+                 new Country("India", "Asia",new List<string> {"DarkYellow","White","DarkGreen"} ),
+                 new Country("Argentina", "South America",new List<string> {"Blue","White"} ),
+                 new Country("Kazakhstan", "Asia",new List<string> {"Blue","Yellow"} ),
+                 new Country("Algeria", "Africa",new List<string> {"Green","White","Red"} ),
+        };
+        public void CountryAction(Country c)
+        {
+            CountryView cv = new CountryView(c);
+            cv.Display();
+        }
+
+
+        public void WelcomeAction()
+        {
+            Console.WriteLine("Hello, welcome to the country app.Please select a country from the following list:");
+            bool runProgram = true;
+            while (runProgram)
+            {
+                CountryListView clv = new CountryListView(CountriesDB);
+                clv.Display();
+                int result = Validator.IntValidator(1,CountriesDB.Count);
+                Console.Clear();
+                CountryAction(CountriesDB[result-1]);
+                runProgram=Validator.GetContinue($"\n\nWould you like to view more countries?");
+            }
+            
+        }
+    }
+}
---
+++ MVC Countries/CountryView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Countries
+{
+    public class CountryView
+    {
+        //Properties
+        public Country DisplayCountry { get; set; }
+        //Constructor
+        public CountryView(Country _displayCountry) 
+        {
+            DisplayCountry = _displayCountry;
+        }
+        //Methods
+        public void Display()
+        {
+            Console.Write($"Name: {DisplayCountry.Name}\nContinent: {DisplayCountry.Continent}\nFlag Colors: ");
+            
+            foreach (string flagColor in DisplayCountry.Colors)
+            {
+                Console.ForegroundColor = Enum.Parse<ConsoleColor>(flagColor);
+                Console.Write($"{flagColor} ");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            
+        }
+
+    }
+}
